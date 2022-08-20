0536883223143b2532fca10d82212f00a2c9cf21
Anwarul Islam <anwaarulislaam@gmail.com>
2022-08-20 16:05:39 +0600

    refactor: ffmpeg path win32

--- controllers/convert.js
+++ controllers/convert.js
@@ -1,39 +1,39 @@
-const fs = require('fs')
-const ffmpeg = require('fluent-ffmpeg')
+const fs = require("fs");
+const ffmpeg = require("fluent-ffmpeg");
 
-const IS_DELETE_ORIGINAL_FILE = true
-const FFMPEG_BINARY_DESTINATION = './lib/ffmpeg-master-latest-win64-gpl/bin/ffmpeg.exe'
+const IS_DELETE_ORIGINAL_FILE = true;
+let FFMPEG_BINARY_DESTINATION = "./lib/ffmpeg/linux/bin/ffmpeg.exe";
 
-ffmpeg.setFfmpegPath(FFMPEG_BINARY_DESTINATION)
+if (process.platform === "win32") {
+  FFMPEG_BINARY_DESTINATION = "./lib/ffmpeg/win/bin/ffmpeg";
+  ffmpeg.setFfmpegPath(FFMPEG_BINARY_DESTINATION);
+}
 
 const convert = (req, res) => {
+  const { formatTo } = req.body;
 
-    const { formatTo } = req.body
+  ffmpeg({ source: req.fileDestination })
+    .toFormat(formatTo)
+    .on("end", () => {
+      console.log("processing done");
+      // delete file from temp folder
+      if (IS_DELETE_ORIGINAL_FILE) {
+        fs.unlink(req.fileDestination, (err) => {
+          if (!err) {
+            console.log("file deleted");
+          }
+        });
+      }
+    })
+    .on("error", (error) => {
+      console.log(error);
+      console.log("some error occured");
+    })
+    .saveToFile("./converted_files/" + req.fileName + "." + formatTo);
 
-    ffmpeg({ source: req.fileDestination })
-        .toFormat(formatTo)
-        .on('end', () => {
-            console.log('processing done')
-            // delete file from temp folder
-            if (IS_DELETE_ORIGINAL_FILE) {
-                fs.unlink(req.fileDestination, (err) => {
-                    if (!err) {
-                        console.log('file deleted')
-                    }
-                })
-            }
-        })
-        .on('error', (error) => {
-            console.log(error)
-            console.log('some error occured')
-        })
-        .saveToFile('./converted_files/' + req.fileName + '.' + formatTo)
-
-    res.send('converting')
-}
-
-
+  res.send("converting");
+};
 
 module.exports = {
-    convert
-}
+  convert,
+};
