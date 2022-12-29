3519164a2014ee5b5cef6177fd1869b2aa9e1e1f
Anwarul Islam <anwaarulislaam@gmail.com>
2022-12-29 19:09:55 +0600

    settings page

--- routes/app.routes.js
+++ routes/app.routes.js
@@ -40,7 +40,7 @@ Router.post("/settings-audio", (req, res) => {
       JSON.stringify(settings)
     );
 
-    res.redirect("/settings-audio");
+    res.redirect("/settings/audio");
   });
 });
 
@@ -78,15 +78,54 @@ Router.post("/settings-rc", (req, res) => {
       JSON.stringify(settings)
     );
 
-    res.redirect("/settings-rc");
+    res.redirect("/settings/rc");
   });
 });
 
-Router.get("/settings-audio", (req, res) => {
-  res.sendFile(path.join(__dirname, "../views/settings-audio.html"));
+Router.post("/settings-bg", (req, res) => {
+  upload(req, res, (err) => {
+    if (err) {
+      console.log(err);
+      return res.status(400).send("Something went wrong!");
+    }
+
+    const jsonFile = "settings-bg.json";
+    let newFilename = "";
+
+    const { file } = req;
+
+    if (file) {
+      const { filename, mimetype } = file;
+      const extension = mimetype.split("/")[1];
+      newFilename = `${Date.now()}.${extension}`;
+      fs.renameSync(
+        path.join(__dirname, `../public/${filename}`),
+        path.join(__dirname, `../public/${newFilename}`)
+      );
+    }
+
+    const settings = require("../public/" + jsonFile);
+
+    if (file) {
+      settings["promoImage"] = "/public/" + newFilename;
+    }
+
+    settings["link"] = req.body.link;
+    fs.writeFileSync(
+      path.join(__dirname, "../public/" + jsonFile),
+      JSON.stringify(settings)
+    );
+
+    res.redirect("/settings/bg");
+  });
 });
-Router.get("/settings-rc", (req, res) => {
-  res.sendFile(path.join(__dirname, "../views/settings-rc.html"));
+
+Router.get("/settings/:id", (req, res) => {
+  const { id } = req.params;
+  if (!fs.existsSync(path.join(__dirname, `../views/settings-${id}.html`))) {
+    return res.status(404).send("Not found");
+  }
+  res.sendFile(path.join(__dirname, `../views/settings-${id}.html`));
 });
 
 Router.get("/embed", (req, res) => {
